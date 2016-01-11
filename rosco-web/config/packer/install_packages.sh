eed2631a2992ce57840304f18c1e9ab8e6398a0c
moondev <chad.d.moon@gmail.com>
2016-01-11 11:14:08 -0700

    support for rpm baking

--- rosco-web/config/packer/install_packages.sh
+++ rosco-web/config/packer/install_packages.sh
@@ -3,6 +3,7 @@
 echo "deb_repo=$deb_repo"
 echo "packages=$packages"
 
+PACKAGE=$packages
 # Strip leading/trailing quotes if present.
 deb_repo=`echo $deb_repo | sed 's/^"\(.*\)"$/\1/'`
 
@@ -12,8 +13,16 @@ packages=`echo $packages | sed 's/^"\(.*\)"$/\1/'`
 
 # Only add the new source repository if deb_repo is set.
 if [[ "$deb_repo" != "" ]]; then
-  echo "deb $deb_repo" | sudo tee /etc/apt/sources.list.d/spinnaker.list > /dev/null
-fi
 
-sudo apt-get update
-sudo apt-get install --force-yes -y $packages
+# If .repo is on the end then we assume it is an rpm, otherwise assume deb.
+if [[ "$deb_repo" == *".repo" ]]
+  then
+    cd /etc/yum.repos.d && sudo wget $deb_repo
+    CMD="sudo yum install -y $packages"
+    eval $CMD
+  else
+    echo "deb $deb_repo" | sudo tee /etc/apt/sources.list.d/spinnaker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install --force-yes -y $packages
+  fi
+fi
